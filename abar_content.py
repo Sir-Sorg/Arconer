89db642fec7fa64fdb82d2ea9633c9fe90913901
Sir-Sorg <sinaorojlo53@gmail.com>
2023-08-02 00:42:01 +0330

    sending sms complete

--- abar_content.py
+++ abar_content.py
@@ -21,7 +21,7 @@ class Classroom:
         self.subject = tableDatas[0].get_text(strip=True)
 
     def content(self):
-        context = f'کلاس تدریس {self.subject} دانش آموز محترم {self.name} برگزار شد و در ساعت {self.time} تاریخ {self.date} به اتمام رسید و کلیه مطالب مشخص شده به خوبی آموزش دیده شد.'
+        context = f'کلاس تدریس {self.subject} دانش آموز محترم {self.name} برگزار شد و در ساعت {self.time} تاریخ {self.date} به اتمام رسید و کلیه مطالب مشخص شده آموزش دیده شد.'
         return context
 
 
@@ -29,7 +29,8 @@ session = requests.Session()
 
 url = "https://code.abarkelas.ir/ppanel/upcoming/"
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    "Origin": "https://code.abarkelas.ir"}
 session.headers.update(headers)
 
 cookies = readCookies()
@@ -44,6 +45,22 @@ print(f'Sended cookies is : {session.cookies}')
 document = BeautifulSoup(response.text, 'html.parser')
 print('='*100)
 # print(document.prettify())
+
+if document.find('input', type='text', attrs={"name": "phone"}):
+    phone = '09391375798'
+    print(f'Cookies was Expierd re-Enter Begining...\nSMS sends to "{phone}"')
+    # B*tchs, you only used one input to verify your identity, you have to reach us
+    csrfmiddlewaretoken = document.find('input', type='hidden', attrs={
+        'name': 'csrfmiddlewaretoken'})
+    csrfmiddlewaretoken = csrfmiddlewaretoken['value']
+    payload = {'csrfmiddlewaretoken': csrfmiddlewaretoken,
+               'phone': phone,
+               'next': '/ppanel'}
+    response = session.post(
+        'https://code.abarkelas.ir/ppanel/login/', data=payload)
+    with open('s.html', 'w') as f:
+        f.write(response.text)
+    print(response.text)
 
 tableRows = document.find_all('tr')
 tableRows = tableRows[1:]  # Remove header
